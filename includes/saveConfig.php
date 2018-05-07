93b052b3ec84a06642dc22e0496cb8a5a886b6d6
Daniel Watson <daniel.watson@nih.gov>
2018-05-07 13:47:30 -0400

    Refactor & auto-format code

--- includes/saveConfig.php
+++ includes/saveConfig.php
@@ -1,39 +1,35 @@
 <?php
-/**
- * Die & Dump Data.
- *
- * @param $data
- */
-function dd($data)
-{
-    echo '<pre>';
-    die(var_dump($data));
-    echo '</pre>';
-}
 
-// Un-escape the string values in the JSON array
-$configData = stripcslashes($_POST['pconfigData']);
+$delimiter = "\t";
 $workFolder = $_POST['workFolder'];
 $externalApp = $_POST['externalApp'];
+$configData = stripcslashes($_POST['pconfigData']);
+$relative_path = "../{$workFolder}config.txt";
+$configData = json_decode($configData, true);
 
-if ($externalApp !== "") {
-    $relative_path = "{$_SERVER['DOCUMENT_ROOT']}/panoromics/{$workFolder}nodes.txt";
-} else {
-    $relative_path = '../' . $workFolder . "config.txt";
+$header = [
+    "rootId", "rootX", "rootY", "timepoint",
+    "charge", "gravity", "borderW", "linkWidth",
+    "linkColor", "maxScore", "minScore", "radius",
+    "fontColorNode", "fontColorCenter", "saved", "key",
+    "colorLabel", "sizeLabel", "typeLabel", "tpLabels",
+    "typeColors", "fillColors", "discreteFillColors", "bgFlg", "background"
+];
+
+$fp = fopen($relative_path, "w");
+
+if ( ! $fp)
+{
+    return false;
 }
 
-//dd($relative_path);
-//dd($_SERVER['DOCUMENT_ROOT']);
-
-// Decode the JSON array
-$configData = json_decode($configData,TRUE);
-
-$header = array("rootId", "rootX", "rootY", "timepoint", "charge", "gravity", "borderW", "linkWidth", "linkColor", "maxScore", "minScore", "radius", "fontColorNode", "fontColorCenter", "saved", "key", "colorLabel", "sizeLabel", "typeLabel", "tpLabels", "typeColors", "fillColors", "discreteFillColors", "bgFlg", "background");
-$delimiter = "\t";
-
-$fp = fopen( $relative_path, "w");
 fputcsv($fp, $header, $delimiter);
-foreach($configData as $row){
+
+foreach ($configData as $row)
+{
     fputcsv($fp, $row, $delimiter);
 }
+
 fclose($fp);
+
+return true;
